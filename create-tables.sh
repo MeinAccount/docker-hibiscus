935a0d4f670c03e270598043bfccc46a927427db
TomMeLi <TomMeLi@users.noreply.github.com>
2019-06-09 14:30:18 +0200

    added ports and ssl setting

--- create-tables.sh
+++ create-tables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-
 DB_PORT=${1:-"3306"}
+
 mysql -h $DB_HOST -P $DB_PORT -u $DB_USERNAME -p $DB_PASSWORD $DB_NAME < /hibiscus-server/plugins/hibiscus/sql/mysql-create.sql
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
+DB_PORT=${1:-"3306"}
+USE_SSL=${1:-"false"}
 
 cat > /hibiscus-server/cfg/de.willuhn.jameica.hbci.rmi.HBCIDBService.properties <<EOF
 database.driver=de.willuhn.jameica.hbci.server.DBSupportMySqlImpl
-database.driver.mysql.jdbcurl=jdbc\:mysql\://${DB_HOST}/${DB_NAME}?useUnicode\=Yes&characterEncoding\=ISO8859_1
+database.driver.mysql.jdbcurl=jdbc\:mysql\://${DB_HOST}:${DB_PORT}/${DB_NAME}?useUnicode\=Yes&characterEncoding\=ISO8859_1
 database.driver.mysql.username=${DB_USERNAME}
 database.driver.mysql.password=${DB_PASSWORD}
 EOF
@@ -12,7 +14,7 @@ cat > /hibiscus-server/cfg/de.willuhn.jameica.webadmin.Plugin.properties <<EOF
 listener.http.address=0.0.0.0
 listener.http.port=8080
 listener.http.auth=true
-listener.http.ssl=false
+listener.http.ssl=$USE_SSL
 EOF
 
 cd /hibiscus-server
