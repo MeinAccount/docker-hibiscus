16da434fedffbfdf9eea2c003adba38f89c109ec
TomMeLi <TomMeLi@users.noreply.github.com>
2019-06-10 20:15:10 +0200

    Update create-tables.sh

--- create-tables.sh
+++ create-tables.sh
@@ -2,4 +2,4 @@
 set -e
 DB_PORT=${1:-"3306"}
 
-mysql -h $DB_HOST -P $DB_PORT -u $DB_USERNAME -p $DB_PASSWORD $DB_NAME < /hibiscus-server/plugins/hibiscus/sql/mysql-create.sql
+mysql -h $DB_HOST -P $DB_PORT -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME < /hibiscus-server/plugins/hibiscus/sql/mysql-create.sql
