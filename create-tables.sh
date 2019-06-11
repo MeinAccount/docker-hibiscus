45ba9dffabbd8eccef806ae0648ea69231ecd4e7
TomMeLi <TomMeLi@users.noreply.github.com>
2019-06-11 17:43:41 +0200

    changing defaulting
    ... finally, I got it. Sorry for the multiple tries

--- create-tables.sh
+++ create-tables.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -e
-mysql --host=$DB_HOST --port=${DB_PORT:=3306} -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME < /hibiscus-server/plugins/hibiscus/sql/mysql-create.sql
+DB_PORT=${DB_PORT:-3306}
+
+mysql --host=$DB_HOST --port=$DB_PORT -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME < /hibiscus-server/plugins/hibiscus/sql/mysql-create.sql
